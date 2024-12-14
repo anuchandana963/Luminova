f146dbe1faa68983ff012937607d24bd64b5773a
anuchandana963 <anuchandana1029@gmail.com>
2024-12-14 10:23:56 +0530

    it a signup page and the otp verification ,and also resend otp

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const express=require('express')
-const app=express();
+//const app=express();
 const path=require("path")
-const env=require('dotenv').config()
+const dotenv=require('dotenv')
+const session=require("express-session");
+dotenv.config()
 const db=require('./config/db');
-const { count } = require('console');
 const userRouter = require("./routes/userRouter")
 db()
+
+const app=express()
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
+app.use(session({
+
+secret:process.env.SESSION_SECRET,
+resave:false,
+saveUninitialized:true,
+cookie:{
+    secure:false,
+    httpOnly:true,
+    maxAge:72*60*60*1000
+}
+}))
+app.use((req,res,next)=>{
+    res.set('cache-control','no-store')
+    next();
+});
+
 
 app.set("view engine","ejs");
 app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
 app.use(express.static(path.join(__dirname,"public")))
 
+
 app.use("/",userRouter)
 
-app.listen(process.env.PORT,()=>{
-    console.log('Server Running');
+const PORT =process.env.PORT || 4000;
+app.listen(PORT,()=>{
+    console.log(`Server Running at ${PORT}`);
 })
 
 module.exports=app;
